d50e4423bc944bfd2d7ee4bddd42e8021a392ad7
akosisaifxD <akosisaif@rocketmail.com>
2016-03-22 20:12:24 +0800

    Update journalentry.php

--- journalentry.php
+++ journalentry.php
@@ -1,24 +1,26 @@
 <?php
 	session_start();
-	if(isset($_POST['siteid'])) {
-		$_SESSION['siteid'] = $_POST['siteid'];
+	if(isset($_POST['sitecode'])) {
+		$_SESSION['sitecode'] = $_POST['sitecode'];
 		$_SESSION['comments'] = $_POST['comments'];
+		$_SESSION['senderid'] = $_POST['senderid'];
 	}
-	$siteid = $_SESSION['siteid'];
+	
+	$sitecode = $_SESSION['sitecode'];
 	$comments = $_SESSION['comments'];
+	$senderid = $_SESSION['senderid'];
+	$sendertype = $_SESSION['sendertype'];
 	
 	include 'connect.php';
 	
 	// prepare and bind
-	$stmt = $conn->prepare("INSERT INTO journal (comments, senderId, siteID, senderType) VALUES (?, ?, ?, ?)");
-	$stmt->bind_param("ssss", $commentsparam, $senderidparam, $siteidparam, $typeparam);
+	$stmt = $conn->prepare("INSERT INTO journal (comments, senderID, siteCode, senderType) VALUES (?, ?, ?, ?)");
+	$stmt->bind_param("ssss", $commentsparam, $senderidparam, $sitecodeparam, $typeparam);
 	$commentsparam = $comments;
-	$senderidparam = 1237890;
-	$siteidparam = $siteid;
-	$typeparam = 0;
+	$senderidparam = $senderid;
+	$sitecodeparam = $sitecode;
+	$typeparam = $sendertype;
 	$stmt->execute();
 	
 	header ("location: journal.php");
-	
-	exit();
 ?>
