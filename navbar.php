f5c70b747fcce20b18f1b0fa7349a2e3767cb273
akosisaifxD <akosisaif@rocketmail.com>
2016-04-04 14:55:42 +0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<style>
+	@font-face {
+		font-family: raleway;
+		src: url(fonts/raleway/Raleway-SemiBold.ttf);
+	}
+
+	body #navbar{
+		background-color: #208f4d;
+		height: 100%;
+		width: 100%;
+		color: white;
+	}
+	
+	#navstat{
+		text-align: center;
+		padding: 10%;
+	}
+	
+	#stattitle{
+		font-family: raleway;
+		font-size: 150%;
+	}
+</style>
+
+<div id = "navbar">
+	<div id = "navstat">
+		<div id = "stattitle"> <i class="fa fa-bar-chart fa-2x"></i> Trends </div>
+	</div>
+	
+</div>
