abb6cb351e2892fc062a440088e9ed4dad30cc9f
akosisaifxD <akosisaif@rocketmail.com>
2016-04-04 14:56:11 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<style>
+	@font-face {
+		font-family: raleway;
+		src: url(fonts/raleway/Raleway-SemiBold.ttf);
+	}
+
+	body #header{
+		background-color: #156234;
+		height: 100%;
+		width: 100%;
+	}
+	
+	#headerlogo{
+		position: absolute;
+		left: 5%;
+		height: 95%;
+	}
+	
+	#headertitle-part-1{
+		font-family: raleway;
+		position: absolute;
+		top: 18%;
+		left: 15%;
+		font-size: 500%;
+		color: white;
+	}
+	
+	#headertitle-part-2{
+		font-family: raleway;
+		position: absolute;
+		top: 31%;
+		left: 16.5%;
+		font-size: 380%;
+		color: white;
+	}
+</style>
+
+<div id = "header">
+	<img src = "img/logo.png" id = "headerlogo" />
+	<div id = "headertitle-part-1"> i </div> <div id = "headertitle-part-2"> PUNO </div>
+</div>
