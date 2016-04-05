a17d96a765ef1575aa2cfc1d0fe4db64d6b458ac
akosisaifxD <akosisaif@rocketmail.com>
2016-04-05 10:36:08 +0800

    Create hvalidation.php

---
+++ hvalidation.php
@@ -0,0 +1,56 @@
+<!-- EXTERNAL SCRIPT CALLS -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+<!-- END OF EXTERNAL SCRIPT CALLS -->
+
+<?php
+	session_start();
+	$_SESSION['cpage'] = 'validation';
+?>
+
+<style>
+	html, body {
+		height: 100%;
+    	margin: 0;
+		background-color: #d5d4d0;
+		width: 100%;
+	}
+	
+	#holdheader{
+		width: 80%;
+		height: 15%;
+		position: fixed;
+		top: 0%;
+		left: 10%;
+		z-index:1;
+	}
+	
+	#holdnav{
+		width: 12%;
+		height:85%;
+		position: fixed;
+		top: 15%;
+		left:10%;
+	}
+	
+	#holdcontent{
+		width: 68%;
+		height: 100%;
+		position: relative;
+		top: 15%;
+		left: 22%;
+	}
+</style>
+
+<div id = "holdheader">
+	<?php include 'header.php' ?>
+</div>
+
+<div id = "holdnav">
+	<?php include 'navbar.php' ?>
+</div>
+
+<div id = "holdcontent">
+	<?php include 'content.php' ?>
+</div>
