cebcecf9f74c2b757ccc521e1ed5b4d915607d03
Haroldold <angharold27@gmail.com>
2016-04-19 17:03:45 +0800

    Added files via upload

--- LoginTest.php
+++ LoginTest.php
@@ -100,7 +100,7 @@
 			
 			$this->assertEquals($test, setPassword());
 		}
-		
+				
 		public function testSetPasswordInvalid(){
 			
 			$_SESSION['username'] = "asdasd";
@@ -175,34 +175,369 @@
 		
 		public function testCheckIfContactPersonFalse(){
 			
-			$un = "2131111";
+			$un = "2232232";
 			
-			if(checkIfContactPerson($un) === 1){
-				$this->assertTrue(false);
+			if(checkIfContactPerson($un) !== true){
+				$this->assertTrue(true);
 			}
 			
 			$un = "4525224";
 			
-			if(checkIfContactPerson($un) === 1){
-				$this->assertTrue(false);
+			if(checkIfContactPerson($un) !== true){
+				$this->assertTrue(true);
 			}
 			
 			$un = "1233412";
 			
-			if(checkIfContactPerson($un) === 1){
-				$this->assertTrue(false);
+			if(checkIfContactPerson($un) !== true){
+				$this->assertTrue(true);
 			}
 			
 			$un = "4241231";
 			
-			if(checkIfContactPerson($un) === 1){
-				$this->assertTrue(false);
+			if(checkIfContactPerson($un) !== true){
+				$this->assertTrue(true);
 			}
 			
 			$un = "4152243";
 			
-			if(checkIfContactPerson($un) === 1){
-				$this->assertTrue(false);
+			if(checkIfContactPerson($un) !== true){
+				$this->assertTrue(true);
+			}
+			
+		}
+		
+		public function testVerifyPasswordForContactPersonValid(){
+			$servername1 = "localhost";
+			$username1 = "root";
+			$password1 = "";
+			$dbname1 = "ipuno";
+			
+			$conn = mysqli_connect($servername1, $username1, $password1, $dbname1);
+
+			$un = "P1";
+			$pw = "ernest";
+			$checker = 0;
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("CPerson",$_SESSION['accounttype']);
+			
+			$un = "P1001";
+			$pw = "placida";
+			$checker = 0;
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("CPerson",$_SESSION['accounttype']);
+			
+			$un = "P10";
+			$pw = "agosto";
+			$checker = 0;
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("CPerson",$_SESSION['accounttype']);
+			
+			$un = "P100";
+			$pw = "antonio";
+			$checker = 0;
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("CPerson",$_SESSION['accounttype']);
+			
+			$un = "P1000";
+			$pw = "pio";
+			$checker = 0;
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("CPerson",$_SESSION['accounttype']);
+			
+		}
+		
+		public function testVerifyPasswordForContactPersonInvalid(){
+			$servername1 = "localhost";
+			$username1 = "root";
+			$password1 = "";
+			$dbname1 = "ipuno";
+			
+			$conn = mysqli_connect($servername1, $username1, $password1, $dbname1);
+
+			$un = "P1";
+			$pw = "ernest1";
+			$checker = 0;
+			$_SESSION['accounttype'] = "";
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "CPerson"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "P1001";
+			$pw = "placida2";
+			$checker = 0;
+			$_SESSION['accounttype'] = "";
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "CPerson"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "P100";
+			$pw = "agosto3";
+			$checker = 0;
+			$_SESSION['accounttype'] = "";
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "CPerson"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "P1000";
+			$pw = "antonio4";
+			$checker = 0;
+			$_SESSION['accounttype'] = "";
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "CPerson"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "P1000";
+			$pw = "pio5";
+			$checker = 0;
+			$_SESSION['accounttype'] = "";
+			
+			verifyPasswordForContactPerson($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "CPerson"){
+				$this->assertFalse(false);
+			}
+			
+		}
+		
+		public function testVerifyPasswordForDENRBasicValid(){
+			$servername2 = "localhost";
+			$username2 = "root";
+			$password2 = "";
+			$dbname2 = "ipuno";
+			
+			$conn = mysqli_connect($servername2, $username2, $password2, $dbname2);
+
+			$un = "1231111";
+			$pw = "jack";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Basic",$_SESSION['accounttype']);
+			
+			$un = "1232222";
+			$pw = "amy";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Basic",$_SESSION['accounttype']);
+			
+			$un = "1232456";
+			$pw = "jane";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Basic",$_SESSION['accounttype']);
+			
+			$un = "1233333";
+			$pw = "sandra";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Basic",$_SESSION['accounttype']);
+			
+			$un = "1234444";
+			$pw = "jon";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Basic",$_SESSION['accounttype']);
+			
+		}
+		
+		public function testVerifyPasswordForDENRBasicInvalid(){
+			$servername2 = "localhost";
+			$username2 = "root";
+			$password2 = "";
+			$dbname2 = "ipuno";
+			
+			$conn = mysqli_connect($servername2, $username2, $password2, $dbname2);
+
+			$un = "1231111";
+			$pw = "jack1";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Basic"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1232222";
+			$pw = "amy2";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Basic"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1232456";
+			$pw = "jane3";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Basic"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1233333";
+			$pw = "sandra4";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Basic"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1234444";
+			$pw = "jon5";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Basic"){
+				$this->assertFalse(false);
+			}
+			
+		}
+		
+		public function testVerifyPasswordForDENRAdvancedValid(){
+			$servername2 = "localhost";
+			$username2 = "root";
+			$password2 = "";
+			$dbname2 = "ipuno";
+			
+			$conn = mysqli_connect($servername2, $username2, $password2, $dbname2);
+
+			$un = "1235678";
+			$pw = "sam";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Advanced",$_SESSION['accounttype']);
+			
+			$un = "1236666";
+			$pw = "nick";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Advanced",$_SESSION['accounttype']);
+			
+			$un = "1236789";
+			$pw = "jay";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Advanced",$_SESSION['accounttype']);
+			
+			$un = "1237777";
+			$pw = "nikki";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Advanced",$_SESSION['accounttype']);
+			
+			$un = "1239012";
+			$pw = "jen";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			$this->assertEquals("Advanced",$_SESSION['accounttype']);
+			
+		}
+		
+		public function testVerifyPasswordForDENRAdvancedInvalid(){
+			$servername2 = "localhost";
+			$username2 = "root";
+			$password2 = "";
+			$dbname2 = "ipuno";
+			
+			$conn = mysqli_connect($servername2, $username2, $password2, $dbname2);
+
+			$un = "1235678";
+			$pw = "sam2";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Advanced"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1236666";
+			$pw = "nick3";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Advanced"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1236789";
+			$pw = "jay4";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Advanced"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1237777";
+			$pw = "nikki5";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Advanced"){
+				$this->assertFalse(false);
+			}
+			
+			$un = "1239012";
+			$pw = "jen6";
+			$checker = 0;
+			
+			verifyPasswordForDENR($un, $pw, $checker, $conn);
+			
+			if($_SESSION['accounttype'] != "Advanced"){
+				$this->assertFalse(false);
 			}
 			
 		}
@@ -210,3 +545,4 @@
 	}
 
 ?>
+
